8945613a5e6bb821a0ceab098d8076f43d1690cf
Diego <diegolara7820@gmail.com>
2024-12-02 02:04:52 -0500

    cambio

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
+import fs from "fs";
+import https from "https";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -16,6 +16,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 dotenv.config({ path: './var.env' }); 
+
+// Lee los archivos del certificado y la clave privada
+const privateKey = fs.readFileSync('/home/ec2-user/server.key', 'utf8');
+const certificate = fs.readFileSync('/home/ec2-user/server.cert', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,16 +33,17 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+});
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// Crea el servidor HTTPS
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
