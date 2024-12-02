8a2668870e1416b9bb3084c15f13e28593b12a5b
Diego <diegolara7820@gmail.com>
2024-12-01 21:36:38 -0500

    correccion

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 dotenv.config({ path: './var.env' }); 
-
+const server = http.createServer(app);
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -28,16 +28,17 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
-if (process.env.NODE_ENV === "production") {
+
+
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+  });
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
+ 
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
